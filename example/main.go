43678b82dcc7a37e6bca96620c2c09b75da31b57
2319676275 <coleman_ward@apple.com>
2018-12-10 00:46:45 -0700

    .

--- example/main.go
+++ example/main.go
@@ -1,45 +1,12 @@
 package main
 
-import (
-	"github.com/gofunct/service"
-	pb "github.com/gofunct/service/test"
-	"go.uber.org/zap"
-	"net"
-)
-
-var (
-	example  *service.Service
-	logger   *zap.Logger
-	listener net.Listener
-)
-
-func init() {
-	var err error
-	logger, err = zap.NewDevelopment()
-	if err != nil {
-		return
-	}
-
-	example = service.NewService(8000, logger, "0.0.0.0", "example")
-}
-
-func Listen(s *service.Service) {
-	defer s.Flush()
-	// Create a new api server.
-	demoServer := pb.NewDemoServer()
-
-	// Register your service.
-	pb.RegisterDemoServiceServer(s.GRPCServer, demoServer)
-
-	s.Run()
-}
-
-func Ping(s *service.Service) {
-	conn := service.NewClientConn(example)
-	go func() { pb.Ping(conn) }()
-}
+import "github.com/gofunct/service"
 
 func main() {
-	Listen(example)
-	Ping(example)
+	service.Execute(
+		service.WithConfig("PWD", "example"),
+		service.WithRootInfo("example", "this is a short descrption"),
+		service.WithServeFunc(":8000"),
+		service.WithDialFunc(":8000"),
+	)
 }
